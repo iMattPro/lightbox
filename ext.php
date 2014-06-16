60d1321caf35f9a51b375914a1a03b9d705629c4
Matt Friedman <maf675@gmail.com>
2014-06-15 23:02:44 -0700

    Update docblocks

--- ext.php
+++ ext.php
@@ -1,9 +1,10 @@
 <?php
 /**
 *
-* @package Lightbox for phpBB 3.1
+* Lightbox extension for the phpBB Forum Software package.
+*
 * @copyright (c) 2014 Matt Friedman
-* @license http://opensource.org/licenses/gpl-2.0.php GNU General Public License v2
+* @license GNU General Public License, version 2 (GPL-2.0)
 *
 */
 
