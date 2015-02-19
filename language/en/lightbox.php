eab46bb7b2067a9b8f667ce866077741ebf3e005
Matt Friedman <maf675@gmail.com>
2015-02-18 19:27:09 -0800

    Add ACP settings with Signature support

---
+++ language/en/lightbox.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ *
+ * Lightbox extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LIGHTBOX_SETTINGS'				=> 'Lightbox settings',
+	'LIGHTBOX_MAX_WIDTH'			=> 'Maximum image width in pixel',
+	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Images that exceed this width will be resized and can be enlarged using the Lightbox effect. Set this value to 0 to disable image resizing.',
+	'LIGHTBOX_GALLERY'				=> 'Allow gallery mode',
+	'LIGHTBOX_GALLERY_EXPLAIN'		=> 'Allows easy navigation between all resized images on the page using the Lightbox effect.',
+	'LIGHTBOX_SIGNATURES'			=> 'Resize signature images',
+	'LIGHTBOX_SIGNATURES_EXPLAIN'	=> 'Allow images used in signatures to be resized.',
+));
---
+++ migrations/m1_add_configs.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Lightbox extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+namespace vse\lightbox\migrations;
+
+class m1_add_configs extends \phpbb\db\migration\migration
+{
+	public function effectively_installed()
+	{
+		return isset($this->config['lightbox_max_width']);
+	}
+
+	static public function depends_on()
+	{
+		return array('\phpbb\db\migration\data\v310\gold');
+	}
+
+	public function update_data()
+	{
+		return array(
+			array('config.add', array('lightbox_max_width', 0)),
+			array('config.add', array('lightbox_gallery', 0)),
+			array('config.add', array('lightbox_signatures', 0)),
+		);
+	}
+}
