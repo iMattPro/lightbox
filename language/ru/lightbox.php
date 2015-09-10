fa5cee19f69f79e000303096ee0289ddc9c7e30d
Matt Friedman <maf675@gmail.com>
2015-09-10 09:17:45 -0700

    Update Russian translation

--- language/ru/lightbox.php
+++ language/ru/lightbox.php
@@ -6,6 +6,13 @@
  * @copyright (c) 2015 Matt Friedman
  * @license GNU General Public License, version 2 (GPL-2.0)
  *
+ * Translation info
+ *
+ * Vasily Batishchev, e-mail: piatachki@gmail.com
+ *
+ * You can found actual versions of phpBB extensions translation here:
+ * http://hooleegan.ru/viewforum.php?f=25
+ *
  */
 
 /**
@@ -24,9 +31,9 @@ if (empty($lang) || !is_array($lang))
 $lang = array_merge($lang, array(
 	'LIGHTBOX_SETTINGS'				=> 'Настройки Lightbox',
 	'LIGHTBOX_MAX_WIDTH'			=> 'Максимальная ширина изображения в пикселях',
-	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Изображения, которые выходят за указанный размер будут уменьшены, а полная версия будет доступна по щелчку. Что бы отключить эту функцию, поставьте 0',
+	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Изображения, которые выходят за указанный размер будут уменьшены, а полная версия будет доступна по щелчку. Чтобы отключить эту функцию, поставьте 0',
 	'LIGHTBOX_GALLERY'				=> 'Включить режим галереи',
-	'LIGHTBOX_GALLERY_EXPLAIN'		=> 'Включает простую навигацию между уменьшенными изображениями с использованием Lightbox-эффекта.',
+	'LIGHTBOX_GALLERY_EXPLAIN'		=> 'Включает простую навигацию по уменьшенным избражениям с использованием Lightbox-эффекта.',
 	'LIGHTBOX_SIGNATURES'			=> 'Уменьшать изображения в подписях',
-	'LIGHTBOX_SIGNATURES_EXPLAIN'	=> 'Если включено, то изображеняи в подписях пользователей так же будут уменьшены.',
+	'LIGHTBOX_SIGNATURES_EXPLAIN'	=> 'Если опция включена, то изображения в подписях пользователей так же будут уменьшены средствами LightBox.',
 ));
