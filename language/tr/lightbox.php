cd34a80daf92f7d712d7d921f2eaad141ee23190
Matt Friedman <maf675@gmail.com>
2017-09-20 23:30:41 -0700

    Add Turkish and Vietnamese

---
+++ language/tr/lightbox.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * Lightbox extension for the phpBB Forum Software package.
+ * Turkish translation by ESQARE (http://www.phpbbturkey.com)
+ *
+ * @copyright (c) 2015 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ *
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LIGHTBOX_SETTINGS'				=> 'Lightbox resim yeniden boyutlandırma',
+	'LIGHTBOX_MAX_WIDTH'			=> 'Piksel cinsinden en yüksek resim genişliği',
+	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Bu genişliği aşan resimler yeniden boyutlandırılacaktır ve Lightbox efekti kullanarak büyütülecektir. Lightbox resim yeniden boyutlandırmayı kapatmak için bu değeri 0 olarak ayarlayın.',
+	'LIGHTBOX_GALLERY'				=> 'Galeri moduna izin ver',
+	'LIGHTBOX_GALLERY_EXPLAIN'		=> 'Lightbox efekti kullanarak sayfadaki tüm yeniden boyutlandırılan resimler arasında kolay gezinmeye izin verir.',
+	'LIGHTBOX_SIGNATURES'			=> 'İmza resimlerini yeniden boyutlandır',
+	'LIGHTBOX_SIGNATURES_EXPLAIN'	=> 'İmzalarda kullanılan resimleri yeniden boyutlandırmaya izin verir.',
+	'LIGHTBOX_IMG_TITLES'			=> 'Resim dosya adlarını göster',
+	'LIGHTBOX_IMG_TITLES_EXPLAIN'	=> 'Lightbox efekti içerisinde resim adları bir başlık olarak gösterilecektir.',
+));
---
+++ language/vi/lightbox.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ *
+ * Lightbox extension for the phpBB Forum Software package.
+ *
+ * @copyright (c) 2015 Matt Friedman
+ * @license GNU General Public License, version 2 (GPL-2.0)
+ * Vietnamese language pack <giaminhteam@gmail.com>
+ */
+
+/**
+ * DO NOT CHANGE
+ */
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+$lang = array_merge($lang, array(
+	'LIGHTBOX_SETTINGS'				=> 'Lightbox image resizing',
+	'LIGHTBOX_MAX_WIDTH'			=> 'Bề rộng tối đa của ảnh (px)',
+	'LIGHTBOX_MAX_WIDTH_EXPLAIN'	=> 'Hình ảnh vượt quá chiều rộng này sẽ được thay đổi kích thước và có thể được mở rộng bằng hiệu ứng Hộp đèn. Đặt giá trị này là 0 để vô hiệu hóa việc thay đổi kích thước hình ảnh của Lightbox.',
+	'LIGHTBOX_MAX_WIDTH_APPEND'		=> 'Dựa trên cài đặt ảnh đính kèm: %spx',
+	'LIGHTBOX_GALLERY'				=> 'Bật chế độ Gallery',
+	'LIGHTBOX_GALLERY_EXPLAIN'		=> 'Dễ dàng chuyển ảnh qua lại trên trang nhờ sử dụng Lightbox effect.',
+	'LIGHTBOX_SIGNATURES'			=> 'Bật thu nhỏ hình ảnh trong chữ ký',
+	'LIGHTBOX_SIGNATURES_EXPLAIN'	=> 'Thu nhỏ hình ảnh trong chữ ký.',
+	'LIGHTBOX_IMG_TITLES'			=> 'Hiển thị tên file ảnh',
+	'LIGHTBOX_IMG_TITLES_EXPLAIN'	=> 'Tên file ảnh được hiển thị là captions khi phóng to ảnh.',
+));
