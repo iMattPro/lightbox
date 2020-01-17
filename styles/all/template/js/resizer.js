8a835af4939027df0517671d1db1e14ea132ad81
Matt Friedman <maf675@gmail.com>
2020-01-16 16:42:27 -0800

    Reduce image size jumping using CSS

--- styles/all/template/js/resizer.js
+++ styles/all/template/js/resizer.js
@@ -38,20 +38,15 @@
 	}
 
 	function lightboxResizer(elements) {
+		if (isMobile() || (!resizeWideImages() && !resizeTallImages() && !vseLightbox.lightboxAll)) {
+			return;
+		}
 		var $targetImage = elements.find('.postimage'),
 			galleryName = 'post-gallery';
 		if (!vseLightbox.lightboxSig) {
 			$targetImage = $targetImage.not(function() {
 				return $(this).closest('.signature').length > 0;
 			});
-		}
-		if (!isMobile() && (resizeWideImages() || resizeTallImages())) {
-			$targetImage.css({
-				'max-width': (resizeWideImages() ? vseLightbox.resizeWidth + 'px' : 'none'),
-				'max-height': (resizeTallImages() ? vseLightbox.resizeHeight + 'px' : 'none')
-			});
-		} else if (!vseLightbox.lightboxAll || isMobile()) {
-			return;
 		}
 		// enclosing the following in a setTimeout seems to solve issues with
 		// images not being ready and causing $(this).width() to return 0.
